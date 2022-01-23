d00d2b48fa60009439f218a52a8bc2930cc2bffc
luyueti111 <48111387+luyueti111@users.noreply.github.com>
2022-01-23 21:37:31 +0800

    Update wsgi.py

--- wsgi.py
+++ wsgi.py
@@ -1,9 +1,18 @@
 import os
 from dotenv import load_dotenv
-from flask import Flask
 
-dotenv_path = os.path.join(os.path.dirname(__file__), '.env')
+dotenv_path = os.path.join(os.path.dirname(__file__), '.flaskenv')
 if os.path.exists(dotenv_path):
     load_dotenv(dotenv_path)
 
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+
 app = Flask('mmMenu')
+app.config.from_pyfile('settings.py')
+app.jinja_env.trim_blocks = True
+app.jinja_env.lstrip_blocks = True
+
+db = SQLAlchemy(app)
+
+from mmMenu import views, commands
